849d5adbb7756681904e2548f3e81b4f0acfff88
Mariia Paraskun <mparasku@c4b5c1.42berlin.de>
2022-12-22 13:13:21 +0100

    Removed unused dir

--- homework/assert.h
+++
@@ -1,4 +0,0 @@
-
-void assertEqualsInts(int expected, int actual, char* message);
-
-void assertEqualsStrings(char* expected, char* actual);
--- homework/lib.h
+++
@@ -1,8 +0,0 @@
-
-int plus(int a, int b);
-
-int minus(int a, int b);
-
-int times(int a, int b);
-
-int length(char* input);
